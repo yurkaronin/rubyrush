3cb9f7e7095a38da133ecfe58ffa861862e6c008
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2021-05-04 01:04:52 +0300

    Выполнил три задания из курса

---
+++ comparison/222.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+num1 = 54
+num2 = 31
+average = (num1 + num2) / 2
+
+
+puts "Первое число: " + num1.to_s
+puts "Второе число: " + num2.to_s
+puts "Среднее арифметическое: " + average.to_s
