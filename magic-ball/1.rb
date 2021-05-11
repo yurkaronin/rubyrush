aa462e7d8789fa875775749bae58fb5a2f105a19
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2021-05-11 14:51:31 +0300

    Вношу корректировки по исходникам упражнений

--- magic-ball/1.rb
+++ magic-ball/1.rb
@@ -1,4 +1,16 @@
-# «Волшебный шар». Он подскажет ответ на любой жизненный вопрос.
+# XXX/ Этот код необходим только при использовании русских букв на Windows
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+# /XXX
+
+# coding: utf-8
+
 answers = [
 # Положительные:
 "Бесспорно",
